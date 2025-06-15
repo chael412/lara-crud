d40d58231e36d67ae87131bb3e9242e1c87ec910
chael412 <165856017+chael412@users.noreply.github.com>
2025-06-15 13:40:54 +0800

    first commit

---
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dashboard() {
+    return <div>Dashboard yyyyyy</div>;
+}
+
+export default Dashboard;
---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,10 @@
+import { Head, Link } from "@inertiajs/react";
+
+export default function Welcome() {
+    return (
+        <>
+            <Head title="Welcome" />
+            <h1>hello world, heheheh ass</h1>
+        </>
+    );
+}
